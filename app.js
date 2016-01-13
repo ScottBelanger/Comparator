4009c61d725dbf5953e131d9bafbfb0790f15bd7
Zach Hoggard <zhoggard9@gmail.com>
2016-01-12 23:34:03 -0500

    Updated to use HTTP port 80

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ app.get('/', function(req, res) {
    res.send('Hello World!');
 });
 
-app.listen(3000, function() {
-   console.log('Listening on port 3000!');
+app.listen(80, function() {
+   console.log('Listening on port 80!');
 });
