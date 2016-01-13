7f1672420071b8e1a676db8d08043f6d32f68236
Zach Hoggard <zhoggard9@gmail.com>
2016-01-12 23:46:52 -0500

    Fixing 502 Bad Gateway

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-   res.send('Hello World!');
+   res.writeHead(200);
+   res.write("Hello World!");
+   res.end();
 });
 
 app.listen(3000, function() {
