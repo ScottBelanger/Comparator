4f5982c1a7eb937a51aa4524ba3dc7815d53c552
Trevor Crawford <tcrawfo6@uwo.ca>
2016-03-23 21:28:21 -0400

    Removed test code

--- public/controllers/comparisonPage.js
+++ public/controllers/comparisonPage.js
@@ -1,13 +1,1 @@
-var xmlhttp = new XMLHttpRequest();
-var url = "/espi/1_1/resource/Subscription/{subscriptionId}/UsagePoint/{usagePointId}/ElectricPowerUsageSummary";
-
-xmlhttp.onreadystatechange = function() {
-    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-        var myArr = JSON.parse(xmlhttp.responseText);
-        console.log(myArr)
-    }
-};
-xmlhttp.open("GET", url, true);
-xmlhttp.send();
-
 angular.module('comparisonPage', []);
