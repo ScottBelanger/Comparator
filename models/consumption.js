747c5995dcc1bdf25436eb2b27a30a6747dc68ba
Zachary Hoggard <zhoggard9@gmail.com>
2016-03-29 21:45:09 -0400

    Changed login date formatting

--- models/consumption.js
+++ models/consumption.js
@@ -7,9 +7,20 @@ var Consumption = function() {
   this.needsUpdate = true;
 };
 
+// Date Formatting stuff
+Number.prototype.padLeft = function(base,chr) {
+  var len = (String(base || 10).length - String(this).length)+1;
+  return len > 0? new Array(len).join(chr || '0')+this : this;
+};
+
 /* ===== Consumption Methods ===== */
 Consumption.prototype.setPoint = function( time, amount ) {
-  this.time   = time;
+  this.time = [ time.getFullYear(),
+           (time.getMonth()+1).padLeft(),
+           time.getDate().padLeft()].join('-') +
+           ' ' +
+         [ time.getHours().padLeft(),
+           time.getMinutes().padLeft()].join(':');
   this.amount = amount;
 }
 
--- models/cost.js
+++ models/cost.js
@@ -7,9 +7,20 @@ var Cost = function() {
   this.needUpdate = true;
 };
 
+// Date Formatting stuff
+Number.prototype.padLeft = function(base,chr) {
+  var len = (String(base || 10).length - String(this).length)+1;
+  return len > 0? new Array(len).join(chr || '0')+this : this;
+};
+
 /* ===== Cost Methods ===== */
 Cost.prototype.setPoint = function( time, amount ) {
-  this.time   = time;
+  this.time = [ time.getFullYear(),
+           (time.getMonth()+1).padLeft(),
+           time.getDate().padLeft()].join('-') +
+           ' ' +
+         [ time.getHours().padLeft(),
+           time.getMinutes().padLeft()].join(':');
   this.amount = amount;
 };
 
