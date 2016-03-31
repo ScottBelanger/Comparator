d216d844221370ef355a33a3ef3fa2e6f224c68a
Zachary Hoggard <zhoggard9@gmail.com>
2016-03-31 13:43:37 -0400

    Almost done with rate engine page

--- public/controllers/reCSVController.js
+++ public/controllers/reCSVController.js
@@ -4,11 +4,36 @@ angular
 
 function reCSVController($scope, $http) {
 	var csvCtrl = this;
-        csvCtrl.rates = [];
-        csvCtrl.timeOfUse = [];
+        csvCtrl.rateRows = [];
+        csvCtrl.timeOfUseRows = [];
 	
-	csvCtrl.parseCSV = function() {
-		console.log("parse stuff");
-                $scope.$emit('parsedCSV', );
-	}
+	csvCtrl.parseRateCSV = function() {
+		console.log("Rate");
+                var file = document.getElementById('uploadRateCSV').files[0];
+                var reader = new FileReader();
+                reader.onload = function() {
+                  var lines = this.result.split('\n');
+                  for(var line = 0; line < lines.length; line++) {
+                    var csvs = lines[line].split('\ ');
+                    csvCtrl.rateRows.push( { rateName: csvs[0], startDate: csvs[1], endDate: csvs[2], units: csvs[3], rateAmount: csvs[4]} );
+                  }
+                  $scope.$emit('RateRows', csvCtrl.rateRows);
+                }
+                reader.readAsText(file);
+	};
+
+	csvCtrl.parseRateTOUCSV = function() {
+		console.log("TOU");
+                var file = document.getElementById('uploadTOUCSV').files[0];
+                var reader = new FileReader();
+                reader.onload = function() {
+                  var lines = this.result.split('\n');
+                  for(var line = 0; line < lines.length; line++) {
+                    var csvs = lines[line].split('\ ');
+                    csvCtrl.rateRows.push( {rateName: csvs[0], startDate: csvs[1], endDate: csvs[2], peakLevel: csvs[3], startHour: csvs[4], endHour: csvs[5], units: csvs[6], rateAmount: csvs[7], isWeekend: csvs[8]} );
+                  }
+                  $scope.$emit('TOURows', csvCtrl.TOURows);
+                }
+                reader.readAsText(file);
+	};
 }
--- public/controllers/reMasterController.js
+++ public/controllers/reMasterController.js
@@ -4,8 +4,42 @@ angular
 
 function reMasterController($scope, $rootScope, $http) {
 	var mstrCtrl = this;
+        mstrCtrl.RateRows = null;
+        mstrCtrl.TOURows = null;
 	
         $scope.$on('LDCid', function(event, id) {
           mstrCtrl.LDCid = id;
+        });
+
+        $scope.$on('RateRows', function(event, rows) {
+          mstrCtrl.RateRows = rows;
+          for(var i = 0; i < mstrCtrl.RateRows; i++) {
+            mstrCtrl.RateRows[i].LDCid = mstrCtrl.LDCid;
+          }
+          if( mstrCtrl.RateRows != null && mstrCtrl.TOURows != null ) {
+            //Post to server
+            for(var i = 0; i < mstrCtrl.RateRows.length; i++) {
+              $http.post('/addRates', object).then(function(){}, function(response) { console.log(response)});
+            }
+            for(var i = 0; i < mstrCtrl.TOU.length; i++) {
+              $http.post('/addTimeOfUseRates', object).then(function(){}, function(response) { console.log(response)});
+            }
+          }
+        });
+
+        $scope.$on('TOURows', function(event, rows) {
+          mstrCtrl.TOURows = rows;
+          for(var i = 0; i < mstrCtrl.TOURows; i++) {
+            mstrCtrl.TOURows[i].LDCid = mstrCtrl.LDCid;
+          }
+          if( mstrCtrl.RateRows != null && mstrCtrl.TOURows != null ) {
+            //Post to server
+            for(var i = 0; i < mstrCtrl.RateRows.length; i++) {
+              $http.post('/addRates', object).then(function(){}, function(response) { console.log(response)});
+            }
+            for(var i = 0; i < mstrCtrl.TOU.length; i++) {
+              $http.post('/addTimeOfUseRates', object).then(function(){}, function(response) { console.log(response)});
+            }
+          }
         });
 }
