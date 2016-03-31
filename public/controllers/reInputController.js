c8b1ed3083b8d566ff70e8f50d08bee327ffb9b7
Trevor Crawford <tcrawfo6@uwo.ca>
2016-03-30 23:26:12 -0400

    Added a few stuff - need to merge

---
+++ public/controllers/reInputController.js
@@ -0,0 +1,9 @@
+angular
+	.module('rateEnginePage')
+	.controller('reInputController', reInputController); //this is where injection could occur
+
+function rateEngineController($scope, $http) {
+	var reInptCtrl = this;
+	
+	
+}
